66f932ea2286b7fda9350a269aee105e069dfbb1
Greg Garvey <greggarvey@gregs-mbp.hsd1.ca.comcast.net>
2015-08-24 20:33:56 -0700

    stanford indies js cleanup

--- js/stanford_indies.js
+++ js/stanford_indies.js
@@ -4,22 +4,17 @@
     attach: function (context, settings) {
       
       var mosaic = $('#isotope-container');
-      var usedIds = [];
-      var hasDuplicates = false;
       var map = {};
       // Process tiles.
       $('.isotope-element').each( function() {
         var tile = $(this);
-
         // Find duplicate tiles.
-        var id = tile.attr('data-id');
-        
+        var id = tile.attr('data-id');    
         if (map[id] == null){
           map[id] = true;
         } else {
           $(this).remove();
-        }
-        
+        }    
         // Add our tile expander link.
         tile.find('.views-field-title').append('<div class="tile-expander tile-more">Learn more</div>');
        
